d88294724b7f90badcafb045349fc69808ef3c23
Michael Padget <mpadget@comcast.net>
2018-10-05 14:08:31 -0700

    Added user model validations

--- models/User.js
+++ models/User.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-let UserSchema = new mongoose.Schema({
-  username: String,
-  email: String,
+const UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-z0-9]+$/, 'is invalid'], index: true},
+  email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
   bio: String,
   image: String,
   hash: String,
