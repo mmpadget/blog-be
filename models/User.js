ce98e2a6a27f4d7ddfe8e796304b81c24eb9ef5b
Michael Padget <mpadget@comcast.net>
2018-10-08 15:55:14 -0700

    Setup and validate user passwords

--- models/User.js
+++ models/User.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
+const crypto = require('crypto')
 
 const UserSchema = new mongoose.Schema({
-  username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-z0-9]+$/, 'is invalid'], index: true},
-  email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+  username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-z0-9]+$/, 'is invalid'], index: true},
+  email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
   bio: String,
   image: String,
   hash: String,
   salt: String
 }, {timestamp: true})
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'})
+
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crypto.randomBytes(16).toString('hex')
+  this.hash = crypto.pbkdf2Sync(password, this.salt 10000, 512, 'sha512').toString('hex')
+}
+
+UserSchema.methods.validPassword = function(password) {
+  let hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex')
+  return this.hash === hash
+}
 
 mongoose.model('User', UserSchema)
